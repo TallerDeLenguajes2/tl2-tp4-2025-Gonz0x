06ceb9dd8b6d13ad9144ba6bc2b9c76583b2152a
Gonz0x <gonzafigueroa6400@gmail.com>
2025-10-06 19:47:34 -0300

    correcion de errores

--- AccesoADatosCadeteria.cs
+++ AccesoADatosCadeteria.cs
@@ -1,25 +1,19 @@
-namespace CadeteriaWebApi.Models;
-
 using System.Text.Json;
+using CadeteriaAPI.Models;
 public class AccesoADatosCadeteria
 {
-    public Cadeteria? Obtener()
-    {
-        string pathCadeteria = Path.Combine(AppContext.BaseDirectory, "Cadeteria.json");
-            string jsonCadeteria = File.ReadAllText(pathCadeteria);
-            Cadeteria? cadeteria = JsonSerializer.Deserialize<Cadeteria>(jsonCadeteria);
-            // Cargar cadetes
-            var datosCadetes = new AccesoADatosCadetes();
-            var cadetes = datosCadetes.Obtener();
-            cadeteria.AgregarListaCadetes(cadetes);
 
-            // Cargar pedidos
-            var datosPedidos = new AccesoADatosPedidos();
-            var pedidos = datosPedidos.Obtener();
-            cadeteria.AgregarListaPedidos(pedidos);
-
+    public Cadeteria Obtener()
+    {    
+        string archivoJson = "Cadeteria.json";
+        string json = File.ReadAllText(archivoJson);
+        Cadeteria? cadeteria = JsonSerializer.Deserialize<Cadeteria>(json);
+        if(cadeteria == null)
+        {
+            throw new Exception("Error al deserializar cadetería");
+        }else
+        {
             return cadeteria;
-        
+        }
     }
-
 }
--- obj/Debug/net8.0/tl2-tp4-2025-Gonz0x.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp4-2025-Gonz0x.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp4-2025-Gonz0x")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a614b8477aa4961e8077e2884946a48ab3ca8953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de49c6853f03f41327693da92ff64947b1557e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp4-2025-Gonz0x")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp4-2025-Gonz0x")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
